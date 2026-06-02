1ccc8ab4f541fa2dc2f57ee4a6639f4a1d86b442
Sangram-Behera22 <mail.sbsangram@gmail.com>
2026-06-02 08:22:18 +0530

    conf:db configured

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-    console.log(`🚀 ERP Vertex running smoothly with Prisma on http://localhost:${PORT}`);
+    console.log(`ERP Vertex running smoothly with Prisma on http://localhost:${PORT}`);
 });

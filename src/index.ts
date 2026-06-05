82d5dadf352b76180e4b64e566b415a8421a01b1
Sangram-Behera22 <mail.sbsangram@gmail.com>
2026-06-05 17:51:15 +0530

    fix: remove console from entry point file.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import app from './app.js';
 import dotenv from 'dotenv';
 dotenv.config();
 const PORT = process.env.PORT || 4000;
-console.log('hello');
 app.listen(PORT, () => {
     console.log(`ERP Vertex running smoothly with Prisma on http://localhost:${PORT}`);
 });
